dcfee19406ce3944029875d3a9f5515db66160d8
spaceC00kie <kirstie317@gmail.com>
2021-03-10 22:12:54 -0500

    cleaned up

--- express-api/index.js
+++ express-api/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
+var cors = require("cors")
 const PORT = 8080
 
-app.listen(PORT, () => console.log("Boozy testing"))
-
 app.use(express.json())
+app.use(cors())
+
+app.listen(PORT, () => console.log("Boozy testing"))
 
 app.get("/cider", (req, res) => {
   res.status(200).send({
