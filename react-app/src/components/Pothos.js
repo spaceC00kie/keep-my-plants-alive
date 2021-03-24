2fa91562f0da5c83847276bd7e7e04d6407ddaae
spaceC00kie <kirstie317@gmail.com>
2021-03-23 23:55:32 -0400

    picked new gauge style

--- react-app/src/components/Pothos.js
+++ react-app/src/components/Pothos.js
@@ -21,7 +21,13 @@ const Pothos = () => {
       {sensor1} <br />
       {sensor2} <br />
       <button onClick={() => getSoilSensor()}>Update</button>
-      <GaugeChart id="gauge-chart1" />
+      <GaugeChart
+        id="gauge-chart3"
+        nrOfLevels={30}
+        colors={["#FF5F6D", "#FFC371"]}
+        arcWidth={0.3}
+        percent={0.37}
+      />
     </div>
   )
 }
