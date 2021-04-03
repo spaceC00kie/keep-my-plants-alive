924e80e72df878db95b293d5f82f3e74a9f6ccd5
spaceC00kie <kirstie317@gmail.com>
2021-04-03 17:04:07 -0400

    button styling

--- react-app/src/components/Pothos.js
+++ react-app/src/components/Pothos.js
@@ -28,7 +28,7 @@ const Pothos = () => {
   return (
     <div className="gauge">
       
-      <button class="button" onClick={() => getSoilSensor()}>Seriously, check again</button>
+      <button class="button" onClick={() => getSoilSensor()}>Check again</button>
       {isConnected ? "" : <h2 id="connection-status">Failed to Connect!</h2>}
       <h1>Sensor 1</h1>
       <GaugeChart
